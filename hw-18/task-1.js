6e7486fc820a94eba1bd5faff5d4551bcd7e98bb
Mark_Web <130235333+Mark4353@users.noreply.github.com>
2024-12-15 12:33:45 +0200

    fix hw and task 1

---
+++ hw-18/task-1.js
@@ -0,0 +1,27 @@
+const ulListElement = document.querySelector("#categories");
+const ulItemElement = document.querySelector("#categorie");
+const ulItemElement2 = document.querySelector("#categorie2");
+const ulItemElement3 = document.querySelector("#categorie3");
+
+
+
+
+
+console.log("У списку 4 категорії");
+console.log("У списку 3 категорії");
+
+console.log("У списку 5 категорії");
+
+
+
+
+
+
+
+
+
+
+
+console.log(`тварини:4`);
+console.log(`продукти:3`);
+console.log(`програмування:5`);
